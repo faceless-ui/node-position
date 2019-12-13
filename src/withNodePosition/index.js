2429837ee59f6978188ffd8da0661a933eef6559
Jarrod Flesch <jarrodmflesch@gmail.com>
2019-12-12 23:46:57 -0500

    keeps the same logic, reduces the loc and the complexity

--- src/withNodePosition/index.js
+++ src/withNodePosition/index.js
@@ -1,76 +1,70 @@
-import React, { Component, createRef } from 'react';
+import React, { useState, useRef, useEffect, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { withWindowInfo } from '@trbl/react-window-info';
 import { withScrollInfo } from '@trbl/react-scroll-info';
 import NodePositionContext from '../NodePositionProvider/context';
 
 const withNodePosition = (PassedComponent) => {
-  class Node extends Component {
-    constructor(props) {
-      super(props);
+  const Node = (props) => {
+    const { frameOffset } = useContext(NodePositionContext);
 
-      this.nodeRef = createRef();
+    const nodeRef = useRef(null);
+    const [nodeRect, setNodeRect] = useState({
+      width: 0,
+      height: 0,
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+    });
 
-      this.state = {
-        nodeRect: {
-          width: 0,
-          height: 0,
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-        },
-        totalXTrack: 0,
-        totalYTrack: 0,
-        xDistanceToFrame: 0,
-        yDistanceToFrame: 0,
-        xIsInFrame: false,
-        yIsInFrame: false,
-        isInFrame: false,
-        xPercentageInFrame: 0,
-        yPercentageInFrame: 0,
-        totalPercentageInFrame: 0,
-      };
-    }
+    const [totalXTrack, setTotalXTrack] = useState(0);
+    const [totalYTrack, setTotalYTrack] = useState(0);
 
-    componentDidMount() {
-      this.queryNodePosition();
-    }
+    const [xDistanceToFrame, setXDistanceToFrame] = useState(0);
+    const [yDistanceToFrame, setYDistanceToFrame] = useState(0);
+    const [xIsInFrame, setXIsInFrame] = useState(false);
+    const [yIsInFrame, setYIsInFrame] = useState(false);
+    const [isInFrame, setIsInFrame] = useState(false);
 
-    componentDidUpdate(prevProps) {
-      const {
-        scrollInfo: { eventsFired: scrollEvents },
-        windowInfo: { eventsFired: windowEvents },
-      } = this.props;
+    const [xPercentageInFrame, setXPercentageInFrame] = useState(0);
+    const [yPercentageInFrame, setYPercentageInFrame] = useState(0);
+    const [totalPercentageInFrame, setTotalPercentageInFrame] = useState(0);
 
-      const {
-        scrollInfo: { eventsFired: prevScrollEvents },
-        windowInfo: { eventsFired: prevWindowEvents },
-      } = prevProps;
+    const {
+      windowInfo: {
+        width: windowWidth,
+        height: windowHeight,
+        eventsFired: windowEvents,
+      },
+      scrollInfo: {
+        eventsFired: scrollEvents,
+        xDifference,
+        yDifference,
+      },
+    } = props;
 
-      if (windowEvents !== prevWindowEvents) {
-        this.queryNodePosition();
+    useEffect(() => {
+      if (scrollEvents <= 1) {
+        const { current: node } = nodeRef;
+        const DOMRect = node.getBoundingClientRect(); // clientRect, relative to the vieport
+        const { width, height, top, right, bottom, left } = DOMRect;
+        setNodeRect({ width, height, top, right, bottom, left }); // create a new, plain object from the DOMRect object type
+      } else {
+        // TODO: consider adjusting the newNodeRect to account for potential changes in the node dimensions.
+        // i.e. if the node's width or height changed at any point during synthetic tracking, these tracked values become innacurate.
+        // A performance hit for this feature is the necessary use of the clientWidth and clientHeight methods on every scroll.
+        setNodeRect(currentRect => ({
+          ...currentRect,
+          top: currentRect.top - yDifference,
+          right: currentRect.right - xDifference,
+          bottom: currentRect.bottom - yDifference,
+          left: currentRect.left - xDifference,
+        }));
       }
+    }, [scrollEvents, windowEvents, xDifference, yDifference]);
 
-      if (scrollEvents !== prevScrollEvents) {
-        // The getBoundingClientRect received on mount in Chrome is calculated relative to the cached scroll position (if present),
-        // so tracking against it before the first scroll event would lead to desynchronization unless queryNodePosition
-        // is run on the first scroll event, which allows for accurate, safe tracking on all subsequent events.
-        if (scrollEvents <= 1) this.queryNodePosition();
-        else this.trackNodePosition();
-      }
-    }
-
-    interpretNodeRect = (nodeRect) => {
-      const {
-        windowInfo: {
-          width: windowWidth,
-          height: windowHeight,
-        },
-      } = this.props;
-
-      const { frameOffset } = this.context;
-
+    useEffect(() => {
       const {
         width: nodeWidth,
         height: nodeHeight,
@@ -89,100 +83,37 @@ const withNodePosition = (PassedComponent) => {
         left: frameOffset,
       };
 
-      const totalXTrack = frame.width + nodeWidth;
-      const xDistanceToFrame = nodeRight - frame.left; // note: the chosen variable name is not the most semantic (nodeRightToFrameLeftDistance || distanceToFrameXExit)
-      const xPercentageInFrame = ((xDistanceToFrame / totalXTrack) * 100) || 0; // conditional assignment for cases where 0 / 0 === NaN
+      setTotalXTrack(frame.width + nodeWidth);
+      setTotalYTrack(frame.height + nodeHeight);
+      setXDistanceToFrame(nodeRight - frame.left); // note: the chosen variable name is not the most semantic (nodeRightToFrameLeftDistance || distanceToFrameXExit;
+      setYDistanceToFrame(nodeBottom - frame.top); // note: the chosen variable name is not the most semantic (nodeBottomToFrameTopDistance || distanceToFrameYExit;
+      setXPercentageInFrame(((xDistanceToFrame / totalXTrack) * 100) || 0); // conditional assignment for cases where 0 / 0 === Na;
+      setYPercentageInFrame(((yDistanceToFrame / totalYTrack) * 100) || 0); // conditional assignment for cases where 0 / 0 === Na;
+      setTotalPercentageInFrame((xPercentageInFrame + yPercentageInFrame) / 2);
+      setXIsInFrame(nodeRight >= frame.left && nodeLeft <= frame.right);
+      setYIsInFrame(nodeTop <= frame.bottom && nodeBottom >= frame.top);
+      setIsInFrame(xIsInFrame && yIsInFrame);
+    }, [frameOffset, nodeRect, windowWidth, windowHeight, totalXTrack, totalYTrack, xDistanceToFrame, yDistanceToFrame, xPercentageInFrame, yPercentageInFrame, xIsInFrame, yIsInFrame]);
 
-      const totalYTrack = frame.height + nodeHeight;
-      const yDistanceToFrame = nodeBottom - frame.top; // note: the chosen variable name is not the most semantic (nodeBottomToFrameTopDistance || distanceToFrameYExit)
-      const yPercentageInFrame = ((yDistanceToFrame / totalYTrack) * 100) || 0; // conditional assignment for cases where 0 / 0 === NaN
-
-      const totalPercentageInFrame = (xPercentageInFrame + yPercentageInFrame) / 2;
-
-      const xIsInFrame = nodeRight >= frame.left && nodeLeft <= frame.right;
-      const yIsInFrame = nodeTop <= frame.bottom && nodeBottom >= frame.top;
-      const isInFrame = xIsInFrame && yIsInFrame;
-
-      return {
-        totalXTrack,
-        totalYTrack,
-        xDistanceToFrame,
-        yDistanceToFrame,
-        xIsInFrame,
-        yIsInFrame,
-        xPercentageInFrame,
-        yPercentageInFrame,
-        totalPercentageInFrame,
-        isInFrame,
-      };
-    }
-
-    // true positions
-    queryNodePosition = () => {
-      const { current: node } = this.nodeRef;
-
-      if (node) {
-        const DOMRect = node.getBoundingClientRect(); // clientRect, relative to the vieport
-        const { width, height, top, right, bottom, left } = DOMRect;
-        const nodeRect = { width, height, top, right, bottom, left }; // create a new, plain object from the DOMRect object type
-
-        const nodePosition = this.interpretNodeRect(nodeRect);
-
-        this.setState({
+    return (
+      <PassedComponent
+        ref={nodeRef}
+        nodePosition={{
           nodeRect,
-          ...nodePosition,
-        });
-      }
-    }
-
-    // synthetic (calculated) positions
-    trackNodePosition = () => {
-      const {
-        scrollInfo: {
-          xDifference,
-          yDifference,
-        },
-      } = this.props;
-
-      const { nodeRect } = this.state;
-
-      // TODO: consider adjusting the newNodeRect to account for potential changes in the node dimensions.
-      // i.e. if the node's width or height changed at any point during synthetic tracking, these tracked values become innacurate.
-      // A performance hit for this feature is the necessary use of the clientWidth and clientHeight methods on every scroll.
-      const newNodeRect = {
-        ...nodeRect, // inherit width and height
-        top: nodeRect.top - yDifference,
-        right: nodeRect.right - xDifference,
-        bottom: nodeRect.bottom - yDifference,
-        left: nodeRect.left - xDifference,
-      };
-
-      const nodePosition = this.interpretNodeRect(newNodeRect);
-
-      this.setState({
-        nodeRect: newNodeRect,
-        ...nodePosition,
-      });
-    }
-
-    render() {
-      const passedProps = { ...this.props };
-      delete passedProps.windowInfo;
-      delete passedProps.scrollInfo;
-
-      return (
-        <PassedComponent
-          ref={this.nodeRef}
-          nodePosition={{ ...this.state }}
-          {...passedProps}
-        />
-      );
-    }
-  }
-
-  Node.contextType = NodePositionContext;
-
-  Node.defaultProps = {
+          totalXTrack,
+          totalYTrack,
+          xDistanceToFrame,
+          yDistanceToFrame,
+          xIsInFrame,
+          yIsInFrame,
+          isInFrame,
+          xPercentageInFrame,
+          yPercentageInFrame,
+          totalPercentageInFrame,
+        }}
+        {...props}
+      />
+    );
   };
 
   Node.propTypes = {
